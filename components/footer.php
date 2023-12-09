a922f7c57e9f5bff6b454b84580c7bb3a9d08439
Fabiano <fabianocosta.trab@gmail.com>
2023-12-09 17:36:21 -0300

    Projeto integrador

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="text-light py-5 mt-5 bg-custom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5 class="mb-4">Redes Sociais</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light"> <i class="bi bi-facebook"></i> Facebook</a></li>
+                    <li><a href="#" class="text-light"> <i class="bi bi-twitter"></i> Twitter</a></li>
+                    <li><a href="#" class="text-light"> <i class="bi bi-instagram"></i> Instagram</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-4">Contato</h5>
+                <p class="mb-1"><i class="bi bi-envelope-fill"></i> <a href="mailto:contato@infoway.com" class="text-light">contato@infoway.com</a></p>
+                <p><i class="bi bi-telephone-fill"></i> (404) 4002-8922</p>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-4">Informações</h5>
+                <p class="small mb-1"><i class="fas fa-users"></i> Mais de X usuários</p>
+                <p class="small mb-1"><i class="fas fa-utensils"></i> Mais de Y receitas compartilhadas</p>
+                <p class="small"><i class="far fa-bookmark"></i> Mais de Z receitas salvas</p>
+            </div>
+        </div>
+<footer class="mt-5 mb-2">
+    <div class="footer">
+    <p class="text-center">
+        Infoway. Todos os direitos reservados
+    </p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
