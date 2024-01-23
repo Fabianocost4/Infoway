3a40182e9820c5c1699a378ba9f07cddc6355e2f
Fabiano <fabianocosta.trab@gmail.com>
2024-01-23 17:13:15 -0300

    Projeto Final

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <p>
+            <label> Email </label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label> Senha </label>
+            <input type="password" name="senha">
+        </p>
+        <p>
+            <buton type ="submit"> Enviar </buton>
+        </p>
+    </form>
+
+</body>
+
+</html>
